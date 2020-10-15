26da5876102bac8ae0563455a3bf73010b97b163
armenazizbekyan <armen@tidepoollabs.com>
2020-10-15 17:26:43 +0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Coming soon
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Kanva React app
-        </a>
       </header>
     </div>
   );
