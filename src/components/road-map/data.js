8c4750cdc5611544d707f1a9021656f829f9525c
Ararat Tonoyan <tonoyandeveloper@gmail.com>
2020-10-16 19:15:48 +0300

    Quarter fixed

--- src/components/road-map/data.js
+++ src/components/road-map/data.js
@@ -4,7 +4,7 @@ export const data = [
     number: 2,
     title: 'QUARTER 2020',
     data: [
-      'Conceptualization of the idea of kanva',
+      'Conceptualization of the idea of Kanva',
       'Conducting of various research and validation of ideas on various models about defi, staking model, and governance',
     ],
   },
@@ -12,7 +12,7 @@ export const data = [
     number: 3,
     title: 'QUARTER 2020',
     data: [
-      'Brainstorming of kanva-specific ideas by the core team',
+      'Brainstorming of Kanva-specific ideas by the core team',
       'Formation of the team, onboarding developers, marketers, and chief of operations',
       'Conducting of community survey',
       'Smart contract development, web and dapp development, and releasing of the token distribution model',
@@ -20,11 +20,11 @@ export const data = [
   },
   {
     number: 4,
-    title: 'QUARTER 2022',
+    title: 'QUARTER 2020',
     data: [
-      'Beta testing of the kanva solidity smart contract on the ropsten test network, conducting of complete dapps unit tests',
+      'Beta testing of the Kanva solidity smart contract on the ropsten test network, conducting of complete dapps unit tests',
       'Execution of marketing plan, partnerships with established projects',
-      'Launching of the official kanva website',
+      'Launching of the official Kanva website',
       'Presale, uniswap listing & locking of liquidity, centralize exchange listing',
       'Launching of the knv token, nft, and token farming',
     ],
@@ -33,7 +33,7 @@ export const data = [
     number: 1,
     title: 'QUARTER 2021',
     data: [
-      'Launching of the kanva governance platform',
+      'Launching of the Kanva governance platform',
       'Partnership announcement',
       'Interoperability integration, future development',
     ],
