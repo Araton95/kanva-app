a18a9acb991bd652c000261aa31f7a22aa295981
khlid khan <khalid@zees-MacBook-Pro.local>
2020-11-10 04:16:48 +0500

    fixed ssr for build

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,11 +6,14 @@
 
 // You can delete this file if you're not using it
 
+import React from "react";
 import "./src/assets/fonts/style.css";
 // import "./src/styles/global.css";
 import "./node_modules/antd/dist/antd.css";
 import "./node_modules/video-react/dist/video-react.css"; // import css
 import "./node_modules/rodal/lib/rodal.css";
+import Layout from "./src/components/Layout";
+
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
